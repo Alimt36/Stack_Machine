80d3d002057a424aed8b8e3594d49988373497e9
Alimt36 <amtaaheri@gmail.com>
2026-02-05 22:47:17 +0330

    start of the compiler on python

---
+++ tinyc_python/tinyc_in_python.py
@@ -0,0 +1,95 @@
+
+import re as regex
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Code file Load
+#---------------------------------------------------------------------------------------------------------------------------
+path = input("Path of the code : ")
+
+with open(path, "r", encoding="utf-8") as f:
+    code_txt = f.read()
+
+print("-" * 100 )
+print(path)
+print("-" * 100 )
+print(code_txt)
+print("-" * 100 )
+#---------------------------------------------------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------------------------------
+operation_str = ""
+line_end:bool = 0
+
+Invalid_Syntax:bool = 0
+#---------------------------------------------------------------------------------------------------------------------------
+
+
+# #---------------------------------------------------------------------------------------------------------------------------
+# # math operations
+# #---------------------------------------------------------------------------------------------------------------------------
+# for line in code_txt.split('\n'):
+#     line = line.strip() 
+#     if line:  
+#         parts = regex.split(r'([+\-*;])', line)
+#         for chars in parts : 
+#             chars.strip()
+#             if chars :
+#                 try :
+#                     x = int(chars)
+#                     # print(x)
+#                     print(f"PUSHI {x}")
+#                 except :
+#                     if chars != ' ' :
+#                         if   chars == '+' : operation_str = "ADD"
+#                         elif chars == '-' : operation_str = "SUB"
+#                         elif chars == '*' : operation_str = "MUL"
+#                         elif chars == ';' : line_end = 1
+#                         else :
+#                             print("Invalid Syntax") 
+#                             operation_str = "-1"
+#         print(operation_str)
+# #---------------------------------------------------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------------------------------
+# variable defenition
+#---------------------------------------------------------------------------------------------------------------------------
+
+def save_variable ( i , parts , value ) :
+    global Invalid_Syntax
+    if value == None :
+        try :
+            x = int(parts[i+2])
+            print(f"PUSHI {x}")
+            print(f"POP {parts[i]}")
+        except :
+            print("Invalid Syntax") 
+            Invalid_Syntax = 1
+            return
+    else :
+        x = 0
+        print(f"PUSHI {x}")
+        print(f"POP {parts[i]}")
+
+for line in code_txt.split('\n'):
+    line = line.strip() 
+    if line:
+        parts = line.split(' ')
+        if parts[0] == "int" : 
+            for i in range (1 , len(parts)) :
+                try : 
+                    if parts[(i + 1)] == "=" :
+                        save_variable(i , parts , None)
+                        break
+                    elif  parts[(i + 1)] == ";" : 
+                        save_variable(i , parts , 0)
+                        break
+                    else : 
+                        print("Invalid Syntax") 
+                        Invalid_Syntax = 1
+                except : 
+                    print("Invalid Syntax") 
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------
