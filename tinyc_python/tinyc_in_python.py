817645f71a7a76817d997256062d6319e7362c75
Alimt36 <amtaaheri@gmail.com>
2026-02-05 23:46:34 +0330

    sth

--- tinyc_python/tinyc_in_python.py
+++ tinyc_python/tinyc_in_python.py
@@ -4,16 +4,19 @@ import re as regex
 #---------------------------------------------------------------------------------------------------------------------------
 # Code file Load
 #---------------------------------------------------------------------------------------------------------------------------
-path = input("Path of the code : ")
+def load_file () -> str : 
+    path = input("Path of the code : ")
 
-with open(path, "r", encoding="utf-8") as f:
-    code_txt = f.read()
+    with open(path, "r", encoding="utf-8") as f:
+        code_txt = f.read()
 
-print("-" * 100 )
-print(path)
-print("-" * 100 )
-print(code_txt)
-print("-" * 100 )
+    print("-" * 100 )
+    print(path)
+    print("-" * 100 )
+    print(code_txt)
+    print("-" * 100 )
+
+    return code_txt
 #---------------------------------------------------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------------------------------------------------
@@ -24,36 +27,36 @@ Invalid_Syntax:bool = 0
 #---------------------------------------------------------------------------------------------------------------------------
 
 
-# #---------------------------------------------------------------------------------------------------------------------------
-# # math operations
-# #---------------------------------------------------------------------------------------------------------------------------
-# for line in code_txt.split('\n'):
-#     line = line.strip() 
-#     if line:  
-#         parts = regex.split(r'([+\-*;])', line)
-#         for chars in parts : 
-#             chars.strip()
-#             if chars :
-#                 try :
-#                     x = int(chars)
-#                     # print(x)
-#                     print(f"PUSHI {x}")
-#                 except :
-#                     if chars != ' ' :
-#                         if   chars == '+' : operation_str = "ADD"
-#                         elif chars == '-' : operation_str = "SUB"
-#                         elif chars == '*' : operation_str = "MUL"
-#                         elif chars == ';' : line_end = 1
-#                         else :
-#                             print("Invalid Syntax") 
-#                             operation_str = "-1"
-#         print(operation_str)
-# #---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
+# math operations
+#---------------------------------------------------------------------------------------------------------------------------
+def math_operation ( code_txt ) :
+    for line in code_txt.split('\n'):
+        line = line.strip() 
+        if line:  
+            parts = regex.split(r'([+\-*;])', line)
+            for chars in parts : 
+                chars.strip()
+                if chars :
+                    try :
+                        x = int(chars)
+                        # print(x)
+                        print(f"PUSHI {x}")
+                    except :
+                        if chars != ' ' :
+                            if   chars == '+' : operation_str = "ADD"
+                            elif chars == '-' : operation_str = "SUB"
+                            elif chars == '*' : operation_str = "MUL"
+                            elif chars == ';' : line_end = 1
+                            else :
+                                print("Invalid Syntax") 
+                                operation_str = "-1"
+            print(operation_str)
+#---------------------------------------------------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------------------------------------------------
 # variable defenition
 #---------------------------------------------------------------------------------------------------------------------------
-
 def save_variable ( i , parts , value ) :
     global Invalid_Syntax
     if value == None :
@@ -66,30 +69,47 @@ def save_variable ( i , parts , value ) :
             Invalid_Syntax = 1
             return
     else :
-        x = 0
+        x = value
         print(f"PUSHI {x}")
         print(f"POP {parts[i]}")
 
-for line in code_txt.split('\n'):
-    line = line.strip() 
-    if line:
-        parts = line.split(' ')
-        if parts[0] == "int" : 
-            for i in range (1 , len(parts)) :
-                try : 
-                    if parts[(i + 1)] == "=" :
-                        save_variable(i , parts , None)
-                        break
-                    elif  parts[(i + 1)] == ";" : 
-                        save_variable(i , parts , 0)
-                        break
-                    else : 
+def variable_definition( code_txt  ) :
+    global Invalid_Syntax
+    for line in code_txt.split('\n'):
+        line = line.strip() 
+        if line:
+            parts = line.split(' ')
+            if parts[0] == "int" : 
+                for i in range (1 , len(parts)) :
+                    try : 
+                        if parts[(i + 1)] == "=" :
+                            save_variable(i , parts , None)
+                            break
+                        elif  parts[(i + 1)] == ";" : 
+                            save_variable(i , parts , 0)
+                            break
+                        else : 
+                            print("Invalid Syntax") 
+                            Invalid_Syntax = 1
+                    except : 
                         print("Invalid Syntax") 
-                        Invalid_Syntax = 1
-                except : 
-                    print("Invalid Syntax") 
+#---------------------------------------------------------------------------------------------------------------------------
 
 
+#---------------------------------------------------------------------------------------------------------------------------
+# main
+#---------------------------------------------------------------------------------------------------------------------------
+
+def main () : 
+
+    code_txt = load_file()
+
+    for line in code_txt.split('\n'):
+        line = line.strip() 
+
+    variable_definition( code_txt )
+    math_operation( code_txt )
 
 
+main()
 #---------------------------------------------------------------------------------------------------------------------------
